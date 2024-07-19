4af2297efc8e83a3cc36a4f706549601ef623d72
= <=>
2024-07-19 15:31:02 +0000

    initial commit

---
+++ app/src/main/java/com/example/todolist/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.todolist.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF1565C0)
+val Background = Color(0xFFFFFFFF)
+
+val PrimaryDark = Color(0xFF0277BD)
+val BackgroundDark = Color(0xFF111111)
